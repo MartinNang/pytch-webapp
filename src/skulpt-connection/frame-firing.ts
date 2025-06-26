8038752d089681c45629e2367a32046e084a3558
Ben North <ben.north@tcd.ie>
2025-06-26 17:12:35 +0100

    FrameFiringArbiter: Define ctor

--- src/skulpt-connection/frame-firing.ts
+++ src/skulpt-connection/frame-firing.ts
@@ -1,3 +1,5 @@
+const N_INTERVAL_SAMPLES = 20;
+
 export class FrameFiringArbiter {
   intervalSamples: Array<number>;
   samplesHead: number;
@@ -6,4 +8,15 @@ export class FrameFiringArbiter {
   targetTime: number;
   targetFireInterval: number;
   prevOpportunityTime: number | null;
+
+  constructor(targetFps: number) {
+    this.intervalSamples = new Array<number>(N_INTERVAL_SAMPLES);
+    this.intervalSamples.fill(0.0);
+    this.samplesHead = 0;
+    this.nSamples = 0;
+
+    this.targetTime = 0.0;
+    this.targetFireInterval = 1000.0 / targetFps;
+    this.prevOpportunityTime = null;
+  }
 }
