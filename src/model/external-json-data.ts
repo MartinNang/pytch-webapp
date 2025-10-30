9518ae1b733f6a154fea30a938e38c5810a0a0ca
Ben North <ben.north@tcd.ie>
2025-10-30 19:58:37 +0000

    ExternalJsonSlice: Define generic type

--- src/model/external-json-data.ts
+++ src/model/external-json-data.ts
@@ -1,5 +1,15 @@
+import { Action, Thunk } from "easy-peasy";
+
 type ContentFetchState<ContentT> =
   | { state: "idle" }
   | { state: "requesting" }
   | { state: "available"; content: ContentT }
   | { state: "error" };
+
+export type ExternalJsonSlice<ContentT> = {
+  contentFetchState: ContentFetchState<ContentT>;
+  setRequestingContent: Action<ExternalJsonSlice<ContentT>>;
+  setContentFetchError: Action<ExternalJsonSlice<ContentT>>;
+  setContent: Action<ExternalJsonSlice<ContentT>, ContentT>;
+  maybeLoadContent: Thunk<ExternalJsonSlice<ContentT>>;
+};
