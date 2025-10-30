cebcf1e4e76563c342775b149d6748a5c3a6f697
Ben North <ben.north@tcd.ie>
2025-10-30 19:59:37 +0000

    externalJsonSlice(): Define function

--- src/model/external-json-data.ts
+++ src/model/external-json-data.ts
@@ -1,4 +1,6 @@
-import { Action, Thunk } from "easy-peasy";
+import { action, Action, thunk, Thunk } from "easy-peasy";
+import { urlWithinApp } from "../env-utils";
+import { fetchParsedJsonValue } from "../utils";
 
 type ContentFetchState<ContentT> =
   | { state: "idle" }
@@ -13,3 +15,39 @@ export type ExternalJsonSlice<ContentT> = {
   setContent: Action<ExternalJsonSlice<ContentT>, ContentT>;
   maybeLoadContent: Thunk<ExternalJsonSlice<ContentT>>;
 };
+
+export function externalJsonSlice<ContentT>(
+  urlNub: string,
+  contentFromRawObj: (rawObj: unknown) => ContentT
+): ExternalJsonSlice<ContentT> {
+  return {
+    contentFetchState: { state: "idle" },
+
+    setRequestingContent: action((state) => {
+      state.contentFetchState = { state: "requesting" };
+    }),
+    setContentFetchError: action((state) => {
+      state.contentFetchState = { state: "error" };
+    }),
+    setContent: action((state, content) => {
+      state.contentFetchState = { state: "available", content };
+    }),
+
+    maybeLoadContent: thunk(async (actions, _voidPayload, helpers) => {
+      const state = helpers.getState();
+      if (state.contentFetchState.state !== "idle") return;
+
+      actions.setRequestingContent();
+
+      try {
+        const url = urlWithinApp(urlNub);
+        const rawObject = await fetchParsedJsonValue(url);
+        const content = contentFromRawObj(rawObject);
+        actions.setContent(content);
+      } catch (err) {
+        console.error(`error fetching content from "${urlNub}":`, err);
+        actions.setContentFetchError();
+      }
+    }),
+  };
+}
