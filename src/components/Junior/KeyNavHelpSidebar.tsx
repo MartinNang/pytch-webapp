6ec4c9203e202527bec77251e066d7e18c56ee4f
Ben North <ben.north@tcd.ie>
2025-11-03 14:47:52 +0000

    Key: Define local component

--- src/components/Junior/KeyNavHelpSidebar.tsx
+++ src/components/Junior/KeyNavHelpSidebar.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { assertNever } from "../../utils";
 import {
   KeyDescriptor,
 } from "../../model/keyboard-shortcuts-help";
@@ -22,3 +24,33 @@ function joinedList(
   });
   return <>{pieces}</>;
 }
+
+const Key: React.FC<{ keyDescr: KeyDescriptor }> = ({ keyDescr }) => {
+  if (typeof keyDescr === "string") {
+    return <span className="help-key">{keyDescr}</span>;
+  }
+
+  switch (keyDescr.kind) {
+    case "chord": {
+      const pieces = joinedList(keyDescr.keys, "+");
+      return <span className="help-key-chord">{pieces}</span>;
+    }
+    case "alternatives": {
+      const pieces = joinedList(keyDescr.keys, "or");
+      return <span className="help-key-alternatives">{pieces}</span>;
+    }
+    case "respective": {
+      const pieces = joinedList(keyDescr.keys, "/");
+      return <span className="help-key-respective">{pieces}</span>;
+    }
+    case "sequence": {
+      if (keyDescr.keys.length !== 2) {
+        throw new Error("only 2-elt sequences are supported");
+      }
+      const pieces = joinedList(keyDescr.keys, "then");
+      return <span className="help-key-sequence">{pieces}</span>;
+    }
+    default:
+      return assertNever(keyDescr);
+  }
+};
