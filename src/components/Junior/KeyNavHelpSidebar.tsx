8fbaec6ef088f9310a2ede4cb85e75c3b9690fdc
Ben North <ben.north@tcd.ie>
2025-11-03 14:50:35 +0000

    ItemContent: Define local component

--- src/components/Junior/KeyNavHelpSidebar.tsx
+++ src/components/Junior/KeyNavHelpSidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { marked } from "marked";
 import { assertNever } from "../../utils";
+import { Row, Col } from "react-bootstrap";
 import {
   KeyDescriptor,
 } from "../../model/keyboard-shortcuts-help";
@@ -60,4 +61,21 @@ const TextContent: React.FC<{ markdown: string }> = ({ markdown }) => {
   marked.use({ mangle: false, headerIds: false });
   const html = marked.parse(markdown);
   return <div dangerouslySetInnerHTML={{ __html: html }} />;
+};
+
+const ItemContent: React.FC<{ keyDescr: KeyDescriptor; help: string }> = ({
+  keyDescr,
+  help,
+}) => {
+  return (
+    <Row className="ItemContent mb-3">
+      <Col xs={5} className="key-descr text-end pe-1">
+        <Key keyDescr={keyDescr} />
+      </Col>
+      <Col className="help-text-container">
+        <span>:</span>
+        <TextContent markdown={help} />
+      </Col>
+    </Row>
+  );
 };
