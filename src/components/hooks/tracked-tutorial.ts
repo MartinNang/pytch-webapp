c41cd5ba2686f15ec4d37d399e074f2ecba13d27
Ben North <ben.north@tcd.ie>
2025-11-12 14:27:33 +0000

    useMediaLibFilterTag(): Define hook

--- src/components/hooks/tracked-tutorial.ts
+++ src/components/hooks/tracked-tutorial.ts
@@ -14,3 +14,33 @@ export function useMappedTrackedTutorial<Result>(
     return mapTutorial(mTrackedTutorial);
   }, eqResult);
 }
+
+/** Hook to provide the filter-tag for use with the media library (if
+ * this project is following a tutorial, flat or per-method), or `null`
+ * if no filter should be applied because the project is not following a
+ * tutorial. */
+export function useMediaLibFilterTag(): string | null {
+  const mkTag = (slug: string) => `_tutorial/${slug}`;
+  return useStoreState((state) => {
+    // Try the two different ways we might be linked to a tutorial.
+    // TODO: Fix this, ugh.
+
+    // Per-method tutorial?
+    const linkedContentLoadingState =
+      state.activeProject.linkedContentLoadingState;
+    if (linkedContentLoadingState.kind === "succeeded") {
+      const linkedContent = linkedContentLoadingState.content;
+      if (linkedContent.kind === "jr-tutorial") {
+        return mkTag(linkedContent.content.name);
+      }
+    }
+
+    // Flat tutorial?
+    const mTutorial = state.activeProject.project.trackedTutorial;
+    if (mTutorial != null) {
+      return mkTag(mTutorial.content.slug);
+    }
+
+    return null;
+  });
+}
