6cc18fdba8d5a1c6330155c86fd29de80f9e95d3
Ben North <ben.north@tcd.ie>
2025-12-16 14:24:28 +0000

    ExternalLinkIndicator: Define component

--- src/components/DecorativeUnderscore.tsx
+++ src/components/DecorativeUnderscore.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { EmptyProps } from "../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const DecorativeUnderscore: React.FC<EmptyProps> = () => (
   <span aria-hidden={true}>_</span>
 );
+
+export const ExternalLinkIndicator: React.FC<EmptyProps> = () => {
+  return (
+    <FontAwesomeIcon
+      style={{ fontSize: "0.75rem", transform: "translateY(-5px)" }}
+      className="ExternalLinkIndicator"
+      icon="arrow-up-right-from-square"
+    />
+  );
+};
