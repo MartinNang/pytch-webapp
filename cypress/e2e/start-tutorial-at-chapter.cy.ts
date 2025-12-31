7b38289b2110131c7e00d2c46728cd6f02ba61d6
Ben North <ben.north@tcd.ie>
2025-12-31 10:16:50 +0000

    assertTutorialNameIncludes(): Define helper

--- cypress/e2e/start-tutorial-at-chapter.cy.ts
+++ cypress/e2e/start-tutorial-at-chapter.cy.ts
@@ -4,4 +4,11 @@ context("Start jr tutorial at chapter", () => {
   beforeEach(() => {
     cy.pytchResetDatabase();
   });
+
+  function assertTutorialNameIncludes(match: string) {
+    cy.get(".TutorialCard.start-at-chapter .card-title").should(
+      "include.text",
+      match
+    );
+  }
 });
