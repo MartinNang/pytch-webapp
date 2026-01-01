baa1c101e545432a435c98708a3fa5f9659440e4
Ben North <ben.north@tcd.ie>
2026-01-01 17:19:54 +0000

    make_content_v4_flat(): Define helper

--- cypress/fixtures/project-zipfiles/make.sh
+++ cypress/fixtures/project-zipfiles/make.sh
@@ -28,6 +28,13 @@ make_content_v4_jr() {
     unzip -q ../simple-v4-jr.zip
 }
 
+make_content_v4_flat() {
+    rm -rf tmp-content
+    mkdir tmp-content
+    cd tmp-content
+    unzip -q ../v4-print-things.zip
+}
+
 make_zipfile() {
     rm -f ../$1.zip
     zip -qr ../$1.zip *
