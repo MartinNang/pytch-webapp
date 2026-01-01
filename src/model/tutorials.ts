92c2affde1ada971fe4058687d327670e199de1c
Ben North <ben.north@tcd.ie>
2026-01-01 11:58:50 +0000

    createDemoFromTutorial(): Simplify
    Use new feature of jrTutorialCheckpointCreateOptions().

--- src/model/tutorials.ts
+++ src/model/tutorials.ts
@@ -301,17 +301,7 @@ export const tutorialCollection: ITutorialCollection = {
               return { summary, program };
             }
             case "per-method": {
-              const skeletonUrl = `${tutorialSlug}/skeleton-structured-program.json`;
-              const skeleton = await tutorialResourceParsedJson(skeletonUrl);
-              const embodyContext = new EmbodyDemoFromTutorial(tutorialSlug);
-              const structuredProgram = StructuredProgramOps.fromSkeleton(
-                skeleton,
-                embodyContext
-              );
-              const program =
-                PytchProgramOps.fromStructuredProgram(structuredProgram);
-              const assets = await embodyContext.allAddAssetDescriptors();
-              return { summary, program, assets };
+              return jrTutorialCheckpointCreateOptions(tutorialSlug, -1);
             }
             default:
               return assertNever(content.programKind);
