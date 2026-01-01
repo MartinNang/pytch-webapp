8f99e70e4c0f91105ca2d5829a437c8892f9462e
Ben North <ben.north@tcd.ie>
2026-01-01 17:40:32 +0000

    invokeFocusShortcut(): Define helper

--- cypress/e2e/keyboard-navigation/global-steer-focus.cy.ts
+++ cypress/e2e/keyboard-navigation/global-steer-focus.cy.ts
@@ -1,5 +1,15 @@
+import {
+  KeyOrShortcut,
+  realPress,
+} from "./utils";
+
 context("Global focus steering shortcuts", () => {
   beforeEach(() => {
     cy.pytchResetDatabase();
   });
+
+  function invokeFocusShortcut(key: KeyOrShortcut) {
+    realPress("g");
+    realPress(key);
+  }
 });
