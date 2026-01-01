cfd8ddbadb3c27ffb5faa8efa5976348540c5801
Ben North <ben.north@tcd.ie>
2026-01-01 17:41:55 +0000

    Add "specimen link" (flat) test

--- cypress/e2e/keyboard-navigation/global-steer-focus.cy.ts
+++ cypress/e2e/keyboard-navigation/global-steer-focus.cy.ts
@@ -1,4 +1,6 @@
+import { assertInIDE } from "../utils";
 import {
+  assertFocus,
   KeyOrShortcut,
   realPress,
 } from "./utils";
@@ -14,5 +16,23 @@ context("Global focus steering shortcuts", () => {
   }
 
   context("flat IDE", () => {
+    it("specimen link", () => {
+      cy.intercept("GET", "**/_by_content_hash_/1234.zip", {
+        fixture: "lesson-specimens/hello-world-lesson.zip",
+      });
+      cy.pytchTryUploadZipfiles(["v4-flat-linked-to-specimen.zip"]);
+      assertInIDE("flat");
+
+      cy.get('button[data-activity-bar-tab="helpsidebar"]').click();
+      realPress("ArrowDown");
+      realPress("Enter");
+
+      invokeFocusShortcut("c");
+      assertFocus("flat-code-editor");
+      realPress("Escape");
+
+      invokeFocusShortcut("h");
+      assertFocus("specimen-info");
+    });
   });
 });
