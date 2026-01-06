5173a2c5792a44e0b5023ce2aa511951a07b3cc7
Ben North <ben.north@tcd.ie>
2026-01-06 11:39:49 +0000

    L/ContentLoadTaskDescriptor: Add program-kind slot

--- src/model/project.ts
+++ src/model/project.ts
@@ -267,6 +267,7 @@ function assertLinkedContentSucceededOfKind<KindT extends LinkedContentKind>(
 
 type LinkedContentLoadTaskDescriptor = {
   projectId: ProjectId;
+  projectProgramKind: PytchProgramKind;
   linkedContentRef: LinkedContentRef;
 };
 
