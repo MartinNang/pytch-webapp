254d498b3f87690b1e25e9619721536866e86c3b
Ben North <ben.north@tcd.ie>
2026-01-30 14:45:03 +0000

    Define some i18n types and a constant
    Types
    
        I18nParams
        I18nFullyQualifiedKey
        I18nStringSpec
    
    and constant
    
        kEmptySpec

---
+++ src/model/i18n/core-types.ts
@@ -0,0 +1,15 @@
+// The "replace" options is type as "any"; try something a bit richer.
+export type I18nParams = Record<string, unknown> & { count?: number };
+
+type I18nFullyQualifiedKey = { ns: string; key: string };
+
+// keyPart here is because this type might be used in contexts where
+// there are other parts to the key.  In some contexts it might be an
+// error for keyPart to be null.  We might need to revisit this.
+export type I18nStringSpec = {
+  keyPart: string | null;
+  params?: I18nParams;
+  indirectParams?: Record<string, I18nFullyQualifiedKey>;
+};
+
+export const kEmptySpec: I18nStringSpec = { keyPart: null };
