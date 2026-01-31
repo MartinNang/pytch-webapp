4d06e648f7ab9b1f6d1ddd1bd22d2acc9fb96287
Ben North <ben.north@tcd.ie>
2026-01-31 13:55:33 +0000

    AddAssetsModal: Convert to i18n

--- src/components/async-flow-modals/AddAssetsModal.tsx
+++ src/components/async-flow-modals/AddAssetsModal.tsx
@@ -7,21 +7,24 @@ import { settleFunctions } from "../../model/user-interactions/async-user-flow";
 import { assertNever } from "../../utils";
 import { FileProcessingFailure } from "../../model/user-interactions/process-files";
 import { AssetOperationContextOps } from "../../model/asset/core";
+import { useTranslation } from "react-i18next";
 import { AddAssetFailuresList } from "./AddAssetFailuresList";
 
 export const AddAssetsModal = () => {
+  const { t } = useTranslation("assets");
+
   const { fsmState, isSubmittable } = useFlowState((f) => f.addAssetsFlow);
   const setChosenFiles = useFlowActions((f) => f.addAssetsFlow.setChosenFiles);
 
   return asyncFlowModal(fsmState, (activeState) => {
     const { operationContext, chosenFiles } = activeState.runState;
-    const assetPlural = "THINGS"; // I18N-TODO
+    const { scope, assetKind } = operationContext;
 
     switch (activeState.kind) {
       case "awaiting-ack-of-notification": {
         return (
           <AddAssetFailuresList
-            assetKind={operationContext.assetKind}
+            assetKind={assetKind}
             failures={activeState.outcomeNub.failures}
             dismiss={activeState.userAck}
           />
@@ -30,13 +33,19 @@ export const AddAssetsModal = () => {
 
       case "interacting":
       case "attempting": {
+        const keyStem = `add.${assetKind}`;
         const settle = settleFunctions(isSubmittable, activeState);
+
+        const titleText = t(`${keyStem}.interacting.title`);
+        const introText = t(`${keyStem}.interacting.intro`);
+        const buttonText = t(`add.this-device.${scope}.interacting.button`);
+
         return (
           <ChooseFiles
-            titleText={`Add ${assetPlural}`}
-            introText={`Choose ${assetPlural} to add to your project.`}
+            titleText={titleText}
+            introText={introText}
             fileAccept={AssetOperationContextOps.fileAccept(operationContext)}
-            actionButtonText="Add to project"
+            actionButtonText={buttonText}
             status={activeState.kind}
             chosenFiles={chosenFiles}
             setChosenFiles={setChosenFiles}
