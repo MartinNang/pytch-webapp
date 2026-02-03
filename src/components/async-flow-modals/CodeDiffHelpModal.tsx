041e4c0f3d41713eae053754381443b10c687304
Ben North <ben.north@tcd.ie>
2026-02-03 11:41:15 +0000

    CodeDiffHelpModal: Convert to i18n

--- src/components/async-flow-modals/CodeDiffHelpModal.tsx
+++ src/components/async-flow-modals/CodeDiffHelpModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Trans, useTranslation } from "react-i18next";
 import Modal from "react-bootstrap/Modal";
 import RawElement from "../RawElement";
 import { asyncFlowModal } from "../async-flow-modals/utils";
@@ -7,6 +8,7 @@ import { useFlowState } from "../../model";
 import { Card } from "react-bootstrap";
 
 export const CodeDiffHelpModal = () => {
+  const { t } = useTranslation("tutorials");
   const { fsmState, isSubmittable } = useFlowState((f) => f.codeDiffHelpFlow);
 
   return asyncFlowModal(fsmState, (activeState) => {
@@ -22,7 +24,7 @@ export const CodeDiffHelpModal = () => {
         size="lg"
       >
         <Modal.Header closeButton>
-          <Modal.Title>What changes should I make to my code?</Modal.Title>
+          <Modal.Title>{t("flat.code-diff.help-title")}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {samples.unchanged && (
@@ -32,12 +34,7 @@ export const CodeDiffHelpModal = () => {
                 element={samples.unchanged}
               />
               <p>
-                Lines like this{" "}
-                <strong>
-                  help you find the part of your code which needs changing
-                </strong>
-                . The number (to the left of the vertical divider) shows you
-                which line it is, in the code as it is now.
+                <Trans i18nKey="flat.code-diff.unchanged-help" ns="tutorials" />
               </p>
             </Card>
           )}
@@ -48,8 +45,7 @@ export const CodeDiffHelpModal = () => {
                 element={samples.deleted}
               />
               <p>
-                Lines like this show you{" "}
-                <strong>code you need to delete</strong>.
+                <Trans i18nKey="flat.code-diff.deleted-help" ns="tutorials" />
               </p>
             </Card>
           )}
@@ -57,15 +53,12 @@ export const CodeDiffHelpModal = () => {
             <Card body>
               <RawElement className="patch-container" element={samples.added} />
               <p>
-                Lines like this show you <strong>code you need to add</strong>.
-                You can click on the <span className="add-code-icon">+</span>{" "}
-                button to copy the lines of code ready for pasting.
+                <Trans i18nKey="flat.code-diff.added-help" ns="tutorials" />
               </p>
             </Card>
           )}
           <p>
-            Faint “<code>···</code>” at the start of a line show spaces — type
-            these as normal space characters.
+            <Trans i18nKey="flat.code-diff.spaces-help" ns="tutorials" />
           </p>
         </Modal.Body>
       </Modal>
