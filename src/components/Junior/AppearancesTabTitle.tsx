fc0e71c87dad18d881d16348722ea881cf97f1e3
Ben North <ben.north@tcd.ie>
2026-02-03 15:28:19 +0000

    AppearancesTabTitle: Convert to i18n

--- src/components/Junior/AppearancesTabTitle.tsx
+++ src/components/Junior/AppearancesTabTitle.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import classNames from "classnames";
+import { ActorKind } from "../../model/junior/structured-program";
+import { useTranslation } from "react-i18next";
+import { kBothActorKinds } from "../../model/junior/structured-program/actor";
 
 // Ensure that the "Backdrops" or "Costumes" tab is always the same
 // width.  Otherwise we get an annoying jitter as you switch between the
@@ -9,17 +12,19 @@ import classNames from "classnames";
 const allDisplayTitleValues = ["Costumes", "Backdrops"];
 // And then use a tighter type for "value" here?
 type AppearancesTabTitleProps = {
-  value: string;
+  actorKind: ActorKind;
 };
 export const AppearancesTabTitle: React.FC<AppearancesTabTitleProps> = ({
-  value,
+  actorKind,
 }) => {
-  const content = allDisplayTitleValues.map((title) => (
+  const { t } = useTranslation("ide");
+
+  const content = kBothActorKinds.map((ak) => (
     <span
-      key={title}
-      className={classNames("title-option", { isActive: title === value })}
+      key={ak}
+      className={classNames("title-option", { isActive: ak === actorKind })}
     >
-      {title}
+      {t(`per-method.tab-title.actor-properties.appearances.${ak}`)}
     </span>
   ));
   return <div className="AppearancesTabTitle">{content}</div>;
