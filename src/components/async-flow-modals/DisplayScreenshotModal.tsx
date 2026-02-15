ccdacfe15e1957c8a62f177ec95c6aa389832ae3
Ben North <ben.north@tcd.ie>
2026-02-15 10:36:29 +0000

    DisplayScreenshotModal: Convert to i18n

--- src/components/async-flow-modals/DisplayScreenshotModal.tsx
+++ src/components/async-flow-modals/DisplayScreenshotModal.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { stageWidth, stageHeight } from "../../constants";
@@ -11,6 +12,8 @@ import { asyncFlowModal } from "../async-flow-modals/utils";
 import { useFlowState } from "../../model";
 
 export const DisplayScreenshotModal = () => {
+  const { t } = useTranslation("flows");
+  const { t: tCommon } = useTranslation("common");
   const { fsmState, isSubmittable } = useFlowState(
     (f) => f.displayScreenshotFlow
   );
@@ -39,20 +42,20 @@ export const DisplayScreenshotModal = () => {
         centered
       >
         <Modal.Header>
-          <Modal.Title>Screenshot</Modal.Title>
+          <Modal.Title>{t("screenshot.title")}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Right-click on the image to copy or save:</p>
+          <p>{t("screenshot.instruction")}</p>
           <img
             ref={imgRef}
             width={stageWidth}
             height={stageHeight}
-            alt="snapshot of stage"
+            alt={t("screenshot.alt")}
           ></img>
         </Modal.Body>{" "}
         <Modal.Footer>
           <Button variant="primary" onClick={settle.cancel}>
-            OK
+            {tCommon("button.ok")}
           </Button>
         </Modal.Footer>
       </Modal>
