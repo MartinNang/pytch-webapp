679c632a47c09c38a28433d22e7225e9cb7f02de
Ben North <ben.north@tcd.ie>
2026-02-15 10:38:29 +0000

    GenericErrorModal: Convert to i18n

--- src/components/async-flow-modals/GenericErrorModal.tsx
+++ src/components/async-flow-modals/GenericErrorModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { ErrorMessageDisplay } from "../ErrorMessageDisplay";
@@ -12,15 +13,17 @@ export const GenericErrorModal: React.FC<GenericErrorModalProps> = ({
   message,
   onAck,
 }) => {
+  const { t } = useTranslation("errors");
+  const { t: tCommon } = useTranslation("common");
   return (
     <Modal className="GenericErrorModal" show={true} animation={false} centered>
       <Modal.Header>
-        <Modal.Title>Unexpected error</Modal.Title>
+        <Modal.Title>{t("unexpected.title")}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <ErrorMessageDisplay errorMessage={message} />
         <div className="d-flex justify-content-end">
-          <Button onClick={onAck}>OK</Button>
+          <Button onClick={onAck}>{tCommon("button.ok")}</Button>
         </div>
       </Modal.Body>
     </Modal>
