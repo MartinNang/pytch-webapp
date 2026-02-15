4b34ca4c4113bada39fb6462eac7562397eaba6a
Ben North <ben.north@tcd.ie>
2026-02-15 16:43:29 +0000

    DemoFromZipfileURL: Convert to i18n
    And replace some texts with spinners.

--- src/components/DemoFromZipfileURL.tsx
+++ src/components/DemoFromZipfileURL.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { demoURLFromId } from "../storage/zipfile";
 import { useStoreActions, useStoreState } from "../store";
 import { NavBanner } from "./NavBanner";
@@ -7,9 +8,10 @@ import LoadingOverlay from "./LoadingOverlay";
 import { Link } from "./LinkWithinApp";
 import { useParams } from "react-router-dom";
 import { EmptyProps } from "../utils";
-import { Card } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 
 export const DemoFromZipfileURL: React.FC<EmptyProps> = () => {
+  const { t } = useTranslation("tutorials");
   const params = useParams();
   const demoState = useStoreState((state) => state.demoFromZipfileURL.state);
   const boot = useStoreActions((actions) => actions.demoFromZipfileURL.boot);
@@ -37,8 +39,8 @@ export const DemoFromZipfileURL: React.FC<EmptyProps> = () => {
       case "booting":
       case "fetching":
         return (
-          <div className="loading-placeholder">
-            <p>Loading...</p>
+          <div className="loading-placeholder text-center">
+            <Spinner animation="border" className="my-3" />
           </div>
         );
       case "proposing":
@@ -48,12 +50,12 @@ export const DemoFromZipfileURL: React.FC<EmptyProps> = () => {
             <h1>{demoState.projectDescriptor.name}</h1>
             <div className="button-bar">
               <Button
-                title="Try this project"
+                title={t("demo.button.title")}
                 disabled={isCreating}
                 variant="outline-primary"
                 onClick={() => createProject()}
               >
-                Demo
+                {t("demo.button.label")}
               </Button>
             </div>
           </>
@@ -61,8 +63,8 @@ export const DemoFromZipfileURL: React.FC<EmptyProps> = () => {
       case "error":
         return (
           <>
-            <h1>Problem</h1>
-            <p>Sorry, there was a problem fetching the project.</p>
+            <h1>{t("demo.problem.title")}</h1>
+            <p>{t("demo.problem.message")}</p>
           </>
         );
       case "idle":
@@ -70,8 +72,8 @@ export const DemoFromZipfileURL: React.FC<EmptyProps> = () => {
         // this URL even though we've navigate()d with "replace".
         return (
           <p>
-            Your project has been created. Please see{" "}
-            <Link to="/my-projects/">My projects</Link>.
+            {t("demo.created-see")}{" "}
+            <Link to="/my-projects/">{t("demo.my-projects-link")}</Link>.
           </p>
         );
     }
@@ -81,11 +83,11 @@ export const DemoFromZipfileURL: React.FC<EmptyProps> = () => {
     <>
       <NavBanner />
       <div className="TutorialList single-tutorial">
-        <h1>This demo was suggested for you:</h1>
+        <h1>{t("demo.title")}</h1>
         <ul className="tutorial-list demo-only">
           <li>
             <LoadingOverlay show={isCreating}>
-              <p>Creating project for demo...</p>
+              <Spinner animation="border" className="my-3" />
             </LoadingOverlay>
             <Card body className="TutorialCard demo-only">
               {content}
