9fec4706b4854866953566cb0d702a4d1ac0b439
Ben North <ben.north@tcd.ie>
2026-02-15 16:11:37 +0000

    DeleteAssetModal: Convert to i18n
    Also simplify the modal title slightly.

--- src/components/async-flow-modals/DeleteAssetModal.tsx
+++ src/components/async-flow-modals/DeleteAssetModal.tsx
@@ -1,28 +1,24 @@
+import { useTranslation } from "react-i18next";
 import { useFlowState } from "../../model";
 import { GenericConfirmActionModal } from "./GenericConfirmActionModal";
 import { asyncFlowModal } from "./utils";
 
 export const DeleteAssetModal = () => {
+  const { t: tAssets } = useTranslation("assets");
   const { fsmState } = useFlowState((f) => f.deleteAssetFlow);
   return asyncFlowModal(fsmState, (activeFsmState) => {
     const { displayName, operationContext } = activeFsmState.runState;
-    const kindDisplayName = "THING"; // I18N-TODO
-    const scopeDisplayName = operationContext.scope;
+    const { scope, assetKind } = operationContext;
+    const t = (keyNub: string) =>
+      tAssets(`delete.${scope}.${assetKind}.${keyNub}`, {
+        replace: { displayName },
+      });
 
     return (
       <GenericConfirmActionModal
         activeFsmState={activeFsmState}
-        headerContent={
-          <p>
-            Delete {kindDisplayName} “{displayName}” from {scopeDisplayName}?
-          </p>
-        }
-        bodyContent={
-          <p>
-            Are you sure you want to delete the {kindDisplayName} “{displayName}
-            ” from {scopeDisplayName}?
-          </p>
-        }
+        headerContent={<p>{t("title")}</p>}
+        bodyContent={<p>{t("body")}</p>}
       />
     );
   });
