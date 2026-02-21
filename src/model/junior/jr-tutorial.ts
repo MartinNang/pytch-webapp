aacb91b5ad77734c459df3f1065df60fa45f270f
Ben North <ben.north@tcd.ie>
2026-02-21 20:38:27 +0000

    ActorIdentifierOps: Remove (now unused)

--- src/model/junior/jr-tutorial.ts
+++ src/model/junior/jr-tutorial.ts
@@ -14,19 +14,6 @@ import * as z from "zod/mini";
 // short numeric id, or a Uuid, or anything like that.
 type ActorIdentifier = { kind: "stage" } | { kind: "sprite"; name: string };
 
-export class ActorIdentifierOps {
-  static nounPhrase(actorIdentifier: ActorIdentifier): string {
-    switch (actorIdentifier.kind) {
-      case "stage":
-        return "the stage";
-      case "sprite":
-        return `the “${actorIdentifier.name}” sprite`;
-      default:
-        return assertNever(actorIdentifier);
-    }
-  }
-}
-
 type ScriptPath = {
   actor: ActorIdentifier;
   methodName: string; // Not sure whether/how we'll use this.
