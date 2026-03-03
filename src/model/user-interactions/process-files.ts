e3657cd30dfedc0c006c359c82228a350dd73092
Ben North <ben.north@tcd.ie>
2026-03-03 17:15:15 +0000

    FileFailureError: Remove (unused)

--- src/model/user-interactions/process-files.ts
+++ src/model/user-interactions/process-files.ts
@@ -4,9 +4,3 @@ export type FileProcessingFailure = {
   filename: string;
   reason: RawOrI18nStringSpec;
 };
-
-export class FileFailureError extends Error {
-  constructor(readonly fileFailures: Array<FileProcessingFailure>) {
-    super("There was a problem when processing a file or files");
-  }
-}
