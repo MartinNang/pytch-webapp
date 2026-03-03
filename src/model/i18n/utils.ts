dcf38d7204d309573770bf35b2c3269888e9ccf6
Ben North <ben.north@tcd.ie>
2026-03-03 11:03:31 +0000

    resolveRawOrI18n(): Define utility

--- src/model/i18n/utils.ts
+++ src/model/i18n/utils.ts
@@ -1,5 +1,6 @@
 import { i18n } from "i18next";
-import { I18nParams, I18nStringSpec } from "./core-types";
+import { I18nParams, I18nStringSpec, RawOrI18nStringSpec } from "./core-types";
+import { assertNever } from "../../utils";
 
 export function resolveIndirectParams(
   i18n: i18n,
@@ -19,4 +20,25 @@ export function resolveIndirectParams(
   }
 
   return resolvedParams;
+}
+
+/** Resolve the given `spec` into a human-readable string.  If the
+ * `spec` is of kind `"raw"`, it is already human-readable.  If of kind
+ * `"i18n"`, translate the contained `spec.spec`, assuming that its
+ * `keyPart` is the entire i18n key. */
+export function resolveRawOrI18n(
+  i18n: i18n,
+  spec: RawOrI18nStringSpec
+): string {
+  switch (spec.kind) {
+    case "raw":
+      return spec.text;
+    case "i18n": {
+      const params = resolveIndirectParams(i18n, spec.spec);
+      const tOptions = { ns: spec.spec.ns, replace: params };
+      return i18n.t(spec.spec.keyPart, tOptions);
+    }
+    default:
+      return assertNever(spec);
+  }
 }
