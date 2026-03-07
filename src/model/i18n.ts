96ca718c76039e30d2ed1a902a714f98d7119892
Ben North <ben.north@tcd.ie>
2026-03-07 09:32:24 +0000

    Add skeleton "welcome" i18n strings file

--- src/model/i18n.ts
+++ src/model/i18n.ts
@@ -65,6 +65,7 @@ export let i18nContextState: I18nContextState = {
           "projects",
           "tutorials",
           "vm",
+          "welcome",
         ],
         defaultNS: "common",
         fallbackLng: "en",
