5d2e432eae6a1d5c8ac4082b8b05fc245cc8d281
Ben North <ben.north@tcd.ie>
2026-04-14 09:19:06 +0100

    LanguageChooser: Define component

---
+++ src/components/Junior/LanguageChooser.tsx
@@ -0,0 +1,38 @@
+import { Container, ListGroup } from "react-bootstrap";
+import { useStoreActions } from "../../store";
+import { EmptyProps } from "../../utils";
+import { useI18nResolvedLanguage } from "./hooks";
+import { supportedLanguages } from "../../model/i18n";
+import { useTranslation } from "react-i18next";
+
+export const LanguageChooser: React.FC<EmptyProps> = () => {
+  const { t } = useTranslation("ide");
+  const resolvedLanguage = useI18nResolvedLanguage();
+  const setLanguage = useStoreActions(
+    (actions) => actions.i18nContextState.setLanguage
+  );
+
+  const setLanguageFun = (lng: string) => () => setLanguage(lng);
+
+  return (
+    <div className="LanguageChooser gfs__help-content" tabIndex={0}>
+      <Container className="help-text">
+        <h1>{t("language-chooser.header")}</h1>
+        <ListGroup>
+          {supportedLanguages.map((lngDescr) => (
+            <ListGroup.Item
+              key={lngDescr.lngCode}
+              active={lngDescr.lngCode === resolvedLanguage}
+              onClick={setLanguageFun(lngDescr.lngCode)}
+            >
+              {lngDescr.name}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        {/* No need to make this a translation string; it will go away
+            once Gaeilge is available. */}
+        <p className="mt-3">Gaeilge coming soon!</p>
+      </Container>
+    </div>
+  );
+};
