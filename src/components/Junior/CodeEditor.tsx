8c789b885db1f746f23cf02b21a76edf038ee0e2
Ben North <ben.north@tcd.ie>
2026-04-14 12:16:07 +0100

    (Reformat)

--- src/components/Junior/CodeEditor.tsx
+++ src/components/Junior/CodeEditor.tsx
@@ -91,19 +91,20 @@ const ScriptsEditor = () => {
 
   const conjoinedResizeObserver = new ConjoinedResizeObserver(handlerIds);
 
-  useEffect(() => {
-    // Purge map entries for handlers not in this instantiation of editor.
-    aceControllerMap.deleteExcept(handlerIds);
+  useEffect(
+    () => {
+      // Purge map entries for handlers not in this instantiation of editor.
+      aceControllerMap.deleteExcept(handlerIds);
 
-    return () => {
-      conjoinedResizeObserver.disconnect();
-    };
-  },
-  // This is all quite fragile, so leave deps array alone until we can
-  // look again in more depth.
-  //
-  // eslint-disable-next-line @eslint-react/exhaustive-deps
-  [handlerIds]
+      return () => {
+        conjoinedResizeObserver.disconnect();
+      };
+    },
+    // This is all quite fragile, so leave deps array alone until we can
+    // look again in more depth.
+    //
+    // eslint-disable-next-line @eslint-react/exhaustive-deps
+    [handlerIds]
   );
 
   const nHandlers = handlerIds.length;
