fd6e3fc2caae67092bd572cb6a218a12c0f43a63
Ben North <ben.north@tcd.ie>
2026-04-14 12:33:24 +0100

    Update eslint and friends
    Newer version, recommended set of extra packages.  Update config file
    to new format.

---
+++ eslint.config.ts
@@ -0,0 +1,37 @@
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintJs from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.ts", "**/*.ts", "**/*.tsx"],
+
+    extends: [
+      eslintJs.configs.recommended,
+      tseslint.configs.recommended,
+      eslintReact.configs["recommended-typescript"],
+    ],
+
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        // Enable project service for better TypeScript integration
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+
+    rules: {
+      "@eslint-react/no-missing-key": "warn",
+      // The codebase uses "let" to highlight that an object will be
+      // mutated, even if the binding itself is unchanged.
+      "prefer-const": 0,
+      // Allow suitably-named args to be unused.
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+]);
