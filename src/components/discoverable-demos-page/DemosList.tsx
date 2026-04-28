264753382cd802b31eb42bf6057eadc530f618e7
Ben North <ben.north@tcd.ie>
2026-04-28 14:22:09 +0100

    DemosList: Split into separate (local) components
    Large diff but mechanical.

--- src/components/discoverable-demos-page/DemosList.tsx
+++ src/components/discoverable-demos-page/DemosList.tsx
@@ -26,32 +26,18 @@ import { CreateProjectFromDemoModal } from "./CreateProjectFromDemoModal";
  * needs to be some machinery to support a reasonable workflow for
  * publishing new demos. */
 
-export const DemosList: React.FC<EmptyProps> = () => {
-  const focusContext = createFocusContext("my-projects-list");
-
-  const maybeLoadContent = useStoreActions(
-    (actions) => actions.discoverableDemos.fetchedDemos.maybeLoadContent
+const DemosHeader: React.FC<EmptyProps> = () => {
+  return (
+    <div className={"demos-header pt-1 mb-2 border-bottom"}>
+      <h1 className={"row pt-5"}>Demos</h1>
+      <RecommendedDemos />
+    </div>
   );
+};
 
-  useEffect(() => {
-    maybeLoadContent();
-  }, []);
-
-  const contentFetchState = useStoreState(
-    (state) => state.discoverableDemos.fetchedDemos.contentFetchState
-  );
+const DemosSearchField: React.FC<EmptyProps> = () => {
   const searchFilters = useStoreState(
     (state) => state.discoverableDemos.searchFilters
-  );
-  const sortBy = useStoreState((state) => state.discoverableDemos.sortBy);
-  const setSearchTerm = useStoreActions(
-    (actions) => actions.discoverableDemos.searchFilters.setSearchTerm
-  );
-  const setSortBy = useStoreActions(
-    (actions) => actions.discoverableDemos.setSortBy
-  );
-  const setProgramKindSelector = useStoreActions(
-    (actions) => actions.discoverableDemos.searchFilters.setProgramKindSelector
   );
   const setDemoKindSelector = useStoreActions(
     (actions) => actions.discoverableDemos.searchFilters.setDemoKindSelector
@@ -59,163 +45,21 @@ export const DemosList: React.FC<EmptyProps> = () => {
   const searchForDemos = useStoreActions(
     (actions) => actions.discoverableDemos.searchForDemos
   );
+  const setSearchTerm = useStoreActions(
+    (actions) => actions.discoverableDemos.searchFilters.setSearchTerm
 
-  const [activePage, setActivePage] = useState(1);
+  );
 
-  useEffect(() => {
-    document.title = "Pytch: Demos";
-  });
+  const searchTermRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     searchTermRef?.current?.focus();
   }, [searchFilters.searchTerm]);
 
-  const paneRef = React.useRef<HTMLDivElement>(null);
-
-  const itemsPerPage = 10;
-
-  const searchTermRef = useRef<HTMLInputElement>(null);
-
-  const handleChangeProgramKind: ChangeEventHandler<HTMLSelectElement> = (
-    evt
-  ) => {
-    setProgramKindSelector(evt.target.value as PytchProgramKindSelector);
+  const handleChangeDemoKind: ChangeEventHandler<HTMLSelectElement> = (evt) => {
+    setDemoKindSelector(evt.target.value as DemoKindSelector);
     searchForDemos();
   };
-
-  function DemosHeader() {
-    return (
-      <div className={"demos-header pt-1 mb-2 border-bottom"}>
-        <h1 className={"row pt-5"}>Demos</h1>
-        <RecommendedDemos />
-      </div>
-    );
-  }
-
-  function DemosContent() {
-    return (
-      <Container>
-        <Row className={"p-3"}>
-          <DemosSearch />
-        </Row>
-        <Row className={"p-3"}>
-          <DemosResults />
-        </Row>
-      </Container>
-    );
-  }
-
-  function DemosSearch() {
-    const handleChangeSortBy: ChangeEventHandler<HTMLSelectElement> = (evt) => {
-      setSortBy(evt.target.value as SortBy);
-      searchForDemos();
-    };
-
-    return (
-      <>
-        <Form className={"py-3"}>
-          <Container className={"p-0"}>
-            <Row className={"d-flex"}>
-              <Col xs={12} md={5} className={"px-0"}>
-                <DemosSearchField />
-              </Col>
-              <Col xs={12} md={7} className={"ms-auto"}>
-                <Row>
-                  <Form.Group
-                    className={"w-auto ms-auto"}
-                    controlId={"formProgramType"}
-                  >
-                    <Form.Label visuallyHidden={true}>Program type</Form.Label>
-                    <Form.Select
-                      aria-label="Program type"
-                      className={"border-0"}
-                      value={searchFilters.programKindSelector}
-                      onInput={handleChangeProgramKind}
-                    >
-                      <option value={"all"}>Program type</option>
-                      {kPytchProgramKindValues.map((programKind) => (
-                        <option key={programKind} value={programKind}>
-                          {displayProgramKindName(programKind)}
-                        </option>
-                      ))}
-                    </Form.Select>
-                  </Form.Group>
-                  <Form.Group className={"w-auto"}>
-                    <Form.Label visuallyHidden={true}>Sort by</Form.Label>
-                    <Form.Select
-                      aria-label="Sort by"
-                      className={"border-0"}
-                      onInput={handleChangeSortBy}
-                      value={sortBy}
-                    >
-                      {Object.values(kSortByValues).map((sortingOption) => (
-                        <option key={sortingOption} value={sortingOption}>
-                          {displaySortByName(sortingOption)}
-                        </option>
-                      ))}
-                    </Form.Select>
-                  </Form.Group>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
-        </Form>
-      </>
-    );
-  }
-
-  function DemosResults() {
-    switch (contentFetchState.state) {
-      case "idle":
-      case "requesting":
-        return (
-          <div
-            className={
-              "mx-auto mt-5 w-100 h-100 d-flex justify-content-center align-items-center"
-            }
-          >
-            <div className="spinner-container">
-              <Spinner animation="border" />
-            </div>
-          </div>
-        );
-      case "available": {
-        const demosContent = contentFetchState.content;
-
-        return (
-          <>
-            {demosContent.searchResults
-              .slice((activePage - 1) * itemsPerPage, activePage * itemsPerPage)
-              .map((demo) => (
-                <Col key={demo.uuid} xs={12} sm={6} lg={4} className={"mb-5"}>
-                  <DemoCard demo={demo} />
-                </Col>
-              ))}
-            {demosContent.searchResults.length === 0 ? (
-              <Col className={"no-results"}>
-                <p>No demos found.</p>
-              </Col>
-            ) : undefined}
-            <PaginationProvider
-              activePage={activePage}
-              setActivePage={setActivePage}
-              nItems={demosContent.searchResults.length}
-              itemsPerPage={itemsPerPage}
-            />
-          </>
-        );
-      }
-      case "error":
-        return (
-          <>
-            <h1>Problem</h1>
-            <p>Sorry, there was a problem fetching the help information.</p>
-          </>
-        );
-      default:
-        return assertNever(contentFetchState);
-    }
-  }
 
   const handleChangeSearchTerm: ChangeEventHandler<HTMLInputElement> = (
     evt
@@ -224,65 +68,227 @@ export const DemosList: React.FC<EmptyProps> = () => {
     searchForDemos();
   };
 
-  function DemosSearchField() {
-    const handleChangeDemoKind: ChangeEventHandler<HTMLSelectElement> = (
-      evt
-    ) => {
-      setDemoKindSelector(evt.target.value as DemoKindSelector);
-      searchForDemos();
-    };
-
-    return (
-      <Form.Group className="mb-3">
-        <Form.Label visuallyHidden={true}>Search</Form.Label>
-        <Container className={"p-0"}>
-          <Row>
-            <div className="d-flex p-0">
-              <div>
-                <Form.Select
-                  key={"ProjectType"}
-                  value={searchFilters.demoKindSelector}
-                  className={"project-type"}
-                  onInput={handleChangeDemoKind}
-                >
-                  <option value={"all"}>All</option>
-                  {kDemoKindValues.map((demoKind) => (
-                    <option key={demoKind} value={demoKind}>
-                      {displayDemoKindName(demoKind)}
-                    </option>
-                  ))}
-                </Form.Select>
-              </div>
-              <div className="flex-grow-1">
-                <Form.Control
-                  key={"searchField"}
-                  id={"search-field"}
-                  className={"w-100"}
-                  placeholder=""
-                  value={searchFilters.searchTerm}
-                  onInput={handleChangeSearchTerm}
-                  ref={searchTermRef}
-                />
-              </div>
-              <div>
-                <Button
-                  id={"search-button"}
-                  className={"flex-shrink-1"}
-                  onClick={() => searchForDemos()}
-                >
-                  <FontAwesomeIcon icon={"search"} inverse={true} />
-                </Button>
-              </div>
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label visuallyHidden={true}>Search</Form.Label>
+      <Container className={"p-0"}>
+        <Row>
+          <div className="d-flex p-0">
+            <div>
+              <Form.Select
+                key={"ProjectType"}
+                value={searchFilters.demoKindSelector}
+                className={"project-type"}
+                onInput={handleChangeDemoKind}
+              >
+                <option value={"all"}>All</option>
+                {kDemoKindValues.map((demoKind) => (
+                  <option key={demoKind} value={demoKind}>
+                    {displayDemoKindName(demoKind)}
+                  </option>
+                ))}
+              </Form.Select>
             </div>
+            <div className="flex-grow-1">
+              <Form.Control
+                key={"searchField"}
+                id={"search-field"}
+                className={"w-100"}
+                placeholder=""
+                value={searchFilters.searchTerm}
+                onInput={handleChangeSearchTerm}
+                ref={searchTermRef}
+              />
+            </div>
+            <div>
+              <Button
+                id={"search-button"}
+                className={"flex-shrink-1"}
+                onClick={() => searchForDemos()}
+              >
+                <FontAwesomeIcon icon={"search"} inverse={true} />
+              </Button>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </Form.Group>
+  );
+};
+
+const DemosSearch: React.FC<EmptyProps> = () => {
+  const searchFilters = useStoreState(
+    (state) => state.discoverableDemos.searchFilters
+  );
+  const searchForDemos = useStoreActions(
+    (actions) => actions.discoverableDemos.searchForDemos
+  );
+  const sortBy = useStoreState((state) => state.discoverableDemos.sortBy);
+  const setSortBy = useStoreActions(
+    (actions) => actions.discoverableDemos.setSortBy
+  );
+  const setProgramKindSelector = useStoreActions(
+    (actions) => actions.discoverableDemos.searchFilters.setProgramKindSelector
+  );
+
+  const handleChangeSortBy: ChangeEventHandler<HTMLSelectElement> = (evt) => {
+    setSortBy(evt.target.value as SortBy);
+    searchForDemos();
+  };
+  const handleChangeProgramKind: ChangeEventHandler<HTMLSelectElement> = (
+    evt
+  ) => {
+    setProgramKindSelector(evt.target.value as PytchProgramKindSelector);
+    searchForDemos();
+  };
+
+  return (
+    <>
+      <Form className={"py-3"}>
+        <Container className={"p-0"}>
+          <Row className={"d-flex"}>
+            <Col xs={12} md={5} className={"px-0"}>
+              <DemosSearchField />
+            </Col>
+            <Col xs={12} md={7} className={"ms-auto"}>
+              <Row>
+                <Form.Group
+                  className={"w-auto ms-auto"}
+                  controlId={"formProgramType"}
+                >
+                  <Form.Label visuallyHidden={true}>Program type</Form.Label>
+                  <Form.Select
+                    aria-label="Program type"
+                    className={"border-0"}
+                    value={searchFilters.programKindSelector}
+                    onInput={handleChangeProgramKind}
+                  >
+                    <option value={"all"}>Program type</option>
+                    {kPytchProgramKindValues.map((programKind) => (
+                      <option key={programKind} value={programKind}>
+                        {displayProgramKindName(programKind)}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className={"w-auto"}>
+                  <Form.Label visuallyHidden={true}>Sort by</Form.Label>
+                  <Form.Select
+                    aria-label="Sort by"
+                    className={"border-0"}
+                    onInput={handleChangeSortBy}
+                    value={sortBy}
+                  >
+                    {Object.values(kSortByValues).map((sortingOption) => (
+                      <option key={sortingOption} value={sortingOption}>
+                        {displaySortByName(sortingOption)}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+              </Row>
+            </Col>
           </Row>
         </Container>
-      </Form.Group>
-    );
+      </Form>
+    </>
+  );
+};
+
+const DemosResults: React.FC<EmptyProps> = () => {
+  const contentFetchState = useStoreState(
+    (state) => state.discoverableDemos.fetchedDemos.contentFetchState
+  );
+
+  const [activePage, setActivePage] = useState(1);
+
+  const itemsPerPage = 10;
+
+  switch (contentFetchState.state) {
+    case "idle":
+    case "requesting":
+      return (
+        <div
+          className={
+            "mx-auto mt-5 w-100 h-100 d-flex justify-content-center align-items-center"
+          }
+        >
+          <div className="spinner-container">
+            <Spinner animation="border" />
+          </div>
+        </div>
+      );
+    case "available": {
+      const demosContent = contentFetchState.content;
+
+      return (
+        <>
+          {demosContent.searchResults
+            .slice((activePage - 1) * itemsPerPage, activePage * itemsPerPage)
+            .map((demo) => (
+              <Col key={demo.uuid} xs={12} sm={6} lg={4} className={"mb-5"}>
+                <DemoCard demo={demo} />
+              </Col>
+            ))}
+          {demosContent.searchResults.length === 0 ? (
+            <Col className={"no-results"}>
+              <p>No demos found.</p>
+            </Col>
+          ) : undefined}
+          <PaginationProvider
+            activePage={activePage}
+            setActivePage={setActivePage}
+            nItems={demosContent.searchResults.length}
+            itemsPerPage={itemsPerPage}
+          />
+        </>
+      );
+    }
+    case "error":
+      return (
+        <>
+          <h1>Problem</h1>
+          <p>Sorry, there was a problem fetching the help information.</p>
+        </>
+      );
+    default:
+      return assertNever(contentFetchState);
   }
+};
+
+const DemosContent: React.FC<EmptyProps> = () => {
+  return (
+    <Container>
+      <Row className={"p-3"}>
+        <DemosSearch />
+      </Row>
+      <Row className={"p-3"}>
+        <DemosResults />
+      </Row>
+    </Container>
+  );
+};
+
+export const DemosList: React.FC<EmptyProps> = () => {
+  const focusContext = createFocusContext("my-projects-list");
+
+  const maybeLoadContent = useStoreActions(
+    (actions) => actions.discoverableDemos.fetchedDemos.maybeLoadContent
+  );
+
+  const paneRef = React.useRef<HTMLDivElement>(null);
 
   const createProject = useStoreActions(
     (actions) => actions.userConfirmations.createProjectFromDemoFlow.run
   );
+
+  useEffect(() => {
+    maybeLoadContent();
+  }, []);
+
+  useEffect(() => {
+    document.title = "Pytch: Demos";
+  });
 
   return (
     <>
