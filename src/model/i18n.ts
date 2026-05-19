25dcdc0ab1553135edf407e259c3f33448533189
Ben North <ben.north@tcd.ie>
2026-05-19 14:38:14 +0100

    (Reformat)

--- src/model/i18n.ts
+++ src/model/i18n.ts
@@ -55,7 +55,14 @@ export let i18nContextState: I18nContextState = {
       i18next.use(Backend).use(LanguageDetector).use(initReactI18next);
       await i18next.init({
         backend: { loadPath: pathWithinApp("/locales/{{lng}}/{{ns}}.json") },
-        ns: ["common", "assets", "ide", "notable-changes", "tutorials", "vm"],
+        ns: [
+          "common",
+          "assets",
+          "ide",
+          "notable-changes",
+          "tutorials",
+          "vm",
+        ],
         defaultNS: "common",
         fallbackLng: "en",
         debug: true,
