10692082ca872ab5c7f0143ed4a52ccddf7b8210
Martin Nang'ole <NANGOLMJ@tcd.ie>
2026-05-26 14:39:14 +0100

    bootForFlatProgram(): Handle "demo" link

--- src/model/junior/edit-state.ts
+++ src/model/junior/edit-state.ts
@@ -225,6 +225,9 @@ export const editState: EditState = {
           case "specimen":
             actions.expandActivityContent("specimen");
             break;
+          case "demo":
+            actions.expandActivityContent("demo");
+            break;
           default:
             assertNever(linkedContentKind);
         }
