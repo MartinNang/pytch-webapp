80d3f7492e76e7d0b81ca713fa03c3f2d5272876
Ben North <ben.north@tcd.ie>
2026-05-28 18:05:43 +0100

    DemoThumbnailContent: Move component to own file

---
+++ src/components/discoverable-demos-page/DemoThumbnailContent.tsx
@@ -0,0 +1,56 @@
+import React, { RefObject } from "react";
+import {
+  DemoCatalogueEntry,
+  demoThumbnailImageUrl,
+  maybeDemoThumbnailVideoUrl,
+} from "../../model/discoverable-demos";
+import classNames from "classnames";
+import Card from "react-bootstrap/Card";
+
+type DemoThumbnailContentProps = {
+  hover: boolean;
+  setHover: (hover: boolean) => void;
+  demo: DemoCatalogueEntry;
+  videoRef: RefObject<HTMLVideoElement | null>;
+};
+export const DemoThumbnailContent: React.FC<DemoThumbnailContentProps> = ({
+  hover,
+  setHover,
+  demo,
+  videoRef,
+}) => {
+  const mVideoSrc = maybeDemoThumbnailVideoUrl(demo);
+  const hasThumbnailVideo = mVideoSrc != null;
+  const showVideo = hover && hasThumbnailVideo;
+  const showImage = !showVideo;
+
+  return (
+    <>
+      {hasThumbnailVideo ? (
+        <video
+          src={mVideoSrc}
+          controls={false}
+          autoPlay={true}
+          muted={true}
+          className={classNames("h-100 w-100 thumbnail-bg", {
+            showVideo,
+          })}
+          onMouseOver={() => {
+            setHover(true);
+          }}
+          onMouseOut={() => setHover(false)}
+          controlsList="nofullscreen"
+          ref={videoRef}
+          tabIndex={-1}
+        >
+          Your browser does not support the video tag.
+        </video>
+      ) : null}
+      <Card.Img
+        variant={"top"}
+        className={classNames("h-100 thumbnail-bg", { showImage })}
+        src={demoThumbnailImageUrl(demo)}
+      />
+    </>
+  );
+};
