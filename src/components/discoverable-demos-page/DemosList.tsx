145d5f1fe10414101f81bdccefd2d9e2aa49e50e
Ben North <ben.north@tcd.ie>
2026-05-28 18:00:59 +0100

    Drop "Provider" when rendering with context

--- src/components/discoverable-demos-page/DemosList.tsx
+++ src/components/discoverable-demos-page/DemosList.tsx
@@ -288,7 +288,7 @@ export const DemosList: React.FC<EmptyProps> = () => {
   return (
     <>
       <CreateProjectFromDemoModal />
-      <FocusContext.Provider value={focusContext}>
+      <FocusContext value={focusContext}>
         <NavBanner />
         <div className="DemosList" tabIndex={-1} ref={paneRef}>
           <DemosHeader />
@@ -314,7 +314,7 @@ export const DemosList: React.FC<EmptyProps> = () => {
             <DemosContent />
           </FocusGroupContainer>
         </div>
-      </FocusContext.Provider>
+      </FocusContext>
     </>
   );
 };
