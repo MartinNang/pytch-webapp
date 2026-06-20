c4e1feeeae049931adbf4e3ecd0d54e56623a666
Ben North <ben.north@tcd.ie>
2026-06-20 16:23:07 +0100

    DemoCatalogueEntry.latestUuid: Allow null
    A null value indicates that the demo has been deleted altogether.

--- src/model/discoverable-demos-schema.ts
+++ src/model/discoverable-demos-schema.ts
@@ -12,6 +12,10 @@ export const kDemoKindValues = ["game" as const, "snippet" as const];
 export const zDemoKind = z.literal(kDemoKindValues);
 export type DemoKind = z.infer<typeof zDemoKind>;
 
+// The slot `latestUuid` is not yet used.  In due course it will support
+// the UI showing a message like "there is a newer version of this demo:
+// CLICK HERE to explore it".  If `null`, it means the whole demo is no
+// longer in the catalogue of live demos.
 export const zDemoCatalogueEntry = z.strictObject({
   uuid: z.string(),
   displayName: z.string(),
@@ -23,7 +27,7 @@ export const zDemoCatalogueEntry = z.strictObject({
   recommended: z.boolean(),
   thumbnailImageExtension: z.string(),
   thumbnailVideoExtension: z.union([z.null(), z.string()]),
-  latestUuid: z.string(),
+  latestUuid: z.union([z.null(), z.string()]),
 });
 export type DemoCatalogueEntry = z.infer<typeof zDemoCatalogueEntry>;
 
