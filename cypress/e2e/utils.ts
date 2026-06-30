e5f132b8bc8ea4a43ba5e96e292fae9c1612de88
Ben North <ben.north@tcd.ie>
2026-06-30 17:43:21 +0100

    Update number of tutorials for Gardening

--- cypress/e2e/utils.ts
+++ cypress/e2e/utils.ts
@@ -2,7 +2,7 @@ import JSZip from "jszip";
 import { PytchProgramKind } from "../../src/model/pytch-program";
 import { assertNever, promiseAndResolve } from "../../src/utils";
 
-export const kExpNTutorials = 18;
+export const kExpNTutorials = 19;
 export const kExpNMediaLibEntries = 53;
 
 /** Set up request intercepts for a specimen for use in tests. */
